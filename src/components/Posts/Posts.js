2249812238f1e057fc010610774f0c8d57df5b37
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-14 02:36:30 +0600

    added posts and post design

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { CircularProgress, Grid } from "@material-ui/core";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
@@ -7,12 +8,21 @@ const Posts = () => {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
   const classes = useStyles();
-  return (
-    <>
-      <h2>Posts</h2>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems='stretch'
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item xs={12} sm={6} key={post._id}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
