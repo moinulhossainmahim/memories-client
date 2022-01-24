96f37035e83b56445e0522fd5e50392ca354fd64
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-01-25 01:00:21 +0600

    added single post details

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -8,7 +8,19 @@ import {
   FETCH_BY_SEARCH,
   END_LOADING,
   START_LOADING,
+  FETCH_POST,
 } from "../constants/actionTypes";
+
+export const getPost = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchPost(id);
+    dispatch({ type: FETCH_POST, payload: data });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getPosts = (page) => async (dispatch) => {
   try {
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -7,6 +7,7 @@ import {
   FETCH_BY_SEARCH,
   START_LOADING,
   END_LOADING,
+  FETCH_POST,
 } from "../constants/actionTypes";
 
 const posts = (state = { isLoading: true, posts: [] }, action) => {
@@ -20,6 +21,8 @@ const posts = (state = { isLoading: true, posts: [] }, action) => {
       };
     case FETCH_BY_SEARCH:
       return { ...state, posts: action.payload };
+    case FETCH_POST:
+      return { ...state, post: action.payload };
     case CREATE:
       return { ...state, posts: [...state.posts, action.payload] };
     case UPDATE:
